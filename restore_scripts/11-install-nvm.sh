7f9a3e90a707e04dff6bf833fde1d57396258f9e
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2021-05-28 21:43:25 +0200

    Updated nvm install script

--- restore_scripts/11-install-nvm.sh
+++ restore_scripts/11-install-nvm.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash || {
+    output::empty_line
+    output::error "Error installing nvm"
+    output::empty_line
+}
