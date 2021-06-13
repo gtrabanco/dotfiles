5c56020151a2846ed5e5c5487c1485709c1cec31
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2021-06-13 14:28:24 +0200

    Added creation of config file for ntfy

---
+++ restoration_scripts/20-ntfy-pushover.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+if [[ -n "$("$DOTLY_PATH/bin/dot" secrets var NTFY_PUSHOVER_TOKEN)" ]] && [[ ! -f "$HOME/.ntfy.yml" ]]; then 
+
+  echo "backends: [\"pushover\"]" >| "$HOME/.ntfy.yml"
+  echo "pushover: {\"user_key\": \"$("$DOTLY_PATH/bin/dot" secrets var NTFY_PUSHOVER_TOKEN)\"}\"" >> "$HOME/.ntfy.yml"
+fi
