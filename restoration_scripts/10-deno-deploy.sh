ef6a234eb9a4b9636b5b71d1dd65859c793fe122
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2021-07-05 11:55:35 +0200

    Updated sloth to latest version

--- restoration_scripts/10-deno-deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-output::answer "Installing Deno deploy"
-deno install --allow-read --allow-write --allow-env --allow-net --allow-run --no-check -f https://deno.land/x/deploy/deployctl.ts
