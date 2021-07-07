4a705584665b7e6cae033d471f34250f285666d0
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2021-07-07 02:05:36 +0200

    Added script to export gpg keys

--- scripts/gpg/src/sec.sh
+++ scripts/gpg/src/sec.sh
@@ -1,1 +1,105 @@
 #!/usr/bin/env bash
+
+sec::gpg() {
+  if [[ -n "${GPG_BIN:-}" && -x "$GPG_BIN" ]]; then
+    "$GPG_BIN" "$@"
+  elif platform::command_exists gpg2; then
+    GPG_BIN="$(which gpg2)"
+    "$GPG_BIN" "$@"
+  elif platform::command_exists gpg; then
+    GPG_BIN="$(which gpg)"
+    "$GPG_BIN" "$@"
+  else
+    output::error "GPG was not found"
+    exit 4
+  fi
+}
+
+sec::menu() {
+  local PS3
+  [[ $# -lt 1 ]] && return
+
+  PS3="Choose an option: "
+  select opt in "$@"; do
+    echo "$opt"
+    return
+  done
+}
+
+sec::generate_key() {
+  local GPG_VERSION
+  GPG_VERSION="$(sec::gpg --version | head -n1 | awk '{print $3}')"
+
+  case "$(platform::semver_compare "$GPG_VERSION" 2.1.17)" in
+    -1)
+      # Lower than 2.1.17
+      sec::gpg --default-new-key-algo rsa4096 --gen-key
+      ;;
+    *)
+      sec::gpg --full-generate-key
+      ;;
+  esac
+}
+
+sec::has_keybase_pkeys() {
+  platform::command_exists keybase && [[ -n "$(keybase pgp list)" ]]
+}
+
+sec::has_pkeys() {
+  [[ -n "$(sec::gpg --list-secret-keys --keyid-format LONG)" ]]
+}
+
+sec::get_pkeys() {
+  sec::gpg --list-secret-keys --keyid-format LONG | grep "sec" | tr '/' ' ' | sort --reverse | awk '{print $3}'
+}
+
+sec::parse_emails() {
+  if [ -t 0 ]; then
+    printf "%s\n" "${@:-}" | grep -i -o '[A-Z0-9._%+-]\+@[A-Z0-9.-]\+\.[A-Z]\{2,4\}'
+  else
+    grep -i -o '[A-Z0-9._%+-]\+@[A-Z0-9.-]\+\.[A-Z]\{2,4\}' < /dev/stdin
+  fi
+}
+
+sec::sed() {
+  if command -v gsed &> /dev/null; then
+    "$(which gsed)" "$@"
+  elif [[ -f "/usr/local/opt/gnu-sed/libexec/gnubin/sed" ]]; then
+    /usr/local/opt/gnu-sed/libexec/gnubin/sed "$@"
+  elif platform::is_macos; then
+    # Any other BSD should be added to this check
+    "$(which sed)" '' "$@"
+  elif command -v sed &> /dev/null; then
+    "$(which sed)" "$@"
+  else
+    return 1
+  fi
+}
+
+sec::list_private_keys() {
+  sec::gpg --list-secret-keys --keyid-format LONG | grep "^pub" | tr "/" " " | awk '{print $3}'
+}
+
+sec::list_public_keys() {
+  sec::gpg --list-keys --keyid-format LONG | grep "^pub" | tr "/" " " | awk '{print $3}'
+}
+
+sec::check_private_key_exists() {
+  [[ -n "${1:-}" ]] && sec::gpg --list-secret-keys --keyid-format LONG "$1" &>/dev/null
+}
+
+sec::check_public_key_exists() {
+  [[ -n "${1:-}" ]] && sec::gpg --list-keys --keyid-format LONG "$1" &>/dev/null
+}
+
+sec::fzf_keys() {
+  local keys
+  if [[ -t 0 ]]; then
+    keys=("$@")
+  else
+    mapfile -t keys < <(echo "$(</dev/stdin)")
+  fi
+
+  #shellcheck disable=SC2016
+  printf "%s\n" "${keys[@]}" | fzf --header "Choose a key to export" --preview '. "${SLOTH_PATH:-$DOTLY_PATH}/scripts/core/src/_main.sh"; dot::load_library "sec.sh" "gpg"; sec::gpg  --list-secret-keys --keyid-format LONG {} | sec::parse_emails'
+}
