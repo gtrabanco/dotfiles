eda83303b1ef1e2f8989be633a6fb7e335a7947a
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2021-07-22 22:26:56 +0200

    Added git binary

--- shell/exports.sh
+++ shell/exports.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 # Dotly configuration
 export DOTLY_THEME=codely
 export SLOTH_THEME=codely
@@ -14,6 +15,9 @@ export DOTLY_SECRETS_VAR_MACOS_STORE="keychain" # filepath or keychain (only mac
 
 # Do you want to make .Sloth loader 12ms faster?
 export BREW_BIN="/usr/local/bin/brew"
+
+# Git binary we want to use
+export GIT_EXECUTABLE="/usr/local/bin/git"
 
 # Avoid touching any of my dotfiles while installing something
 export PROFILE="/dev/null"
