14a09d101f8aa27d3e6791d9965c33eb7b09b1e8
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2021-07-24 20:28:59 +0200

    Changed theme configuration

--- shell/exports.sh
+++ shell/exports.sh
@@ -2,8 +2,9 @@
 # Dotly configuration
 export DOTLY_THEME=codely
 export SLOTH_THEME=sloth
-export CODELY_THEME_MODE=dark # Dark mode is default one
-export CODELY_THEME_MINIMAL=false
+export SLOTH_THEME_MULTILINE=true
+export SLOTH_THEME_MINIMAL=false
+
 export SLOTH_ENV="development" # Avoids updating if .Sloth path is dirty. Important
 # to not loosing time developing stuff that will ends in /dev/null
 export SLOTH_AUTO_UPDATE_PERIOD_IN_DAYS=7
