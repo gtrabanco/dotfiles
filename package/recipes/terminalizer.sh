27e8194e89a45384002904c6d25af8a0bae0f81a
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2021-08-03 16:29:25 +0200

    terminalizer recipe

---
+++ package/recipes/terminalizer.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+terminalizer::install() {
+  if [[ -r "${SLOTH_PATH:-${DOTLY_PATH:-}}/shell/init.scripts/nvm" ]]; then
+    #shellcheck disable=SC1091
+    . "${SLOTH_PATH:-${DOTLY_PATH:-}}/shell/init.scripts/nvm"
+  fi
+  script::depends_on nvm
+
+  if paltform::command_exists yum && sudo -v -B; then
+    sudo yum install -y libXScrnSaver
+  fi
+
+  if paltform::command_exists apt-get && sudo -v -B; then
+    sudo apt-get install -y libgconf-2-4
+  fi
+
+  npm install -g terminalizer
+
+  terminalizer::is_installed && output::answer "\`terminalizer\` installed"
+}
+
+terminalizer::is_installed() {
+  paltform::command_exists terminalizer
+}
