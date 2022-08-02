81cf6dfed669ebe0d159f4cd42919290e9e68b8d
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2022-08-03 00:13:44 +0200

    Bun at the end

--- shell/paths.sh
+++ shell/paths.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
+
 export path=(
   "${HOME}/bin"
   "${DOTFILES_PATH}/bin"
   "${HOME}/.yarn/bin"
   "${HOME}/.config/yarn/global/node_modules/.bin"
-  "${HOME}/.bun/bin"
   "${HOME}/.local/bin"
+  "${HOME}/.bun/bin"
 )
