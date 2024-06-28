9940b747f7f891e1d076b73fe6454fd53d9c03b7
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2024-06-28 12:20:25 +0200

    podman as docker, some init scripts

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -117,3 +117,4 @@ alias jq="yq -p=json"
 alias kb="dot kb flash" #Flash ergodox
 alias icloud="cd '${HOME}/Library/Mobile\ Documents/com~apple~CloudDocs/'"
 alias tailscale="/Applications/Tailscale.app/Contents/MacOS/Tailscale"
+alias docker=podman
